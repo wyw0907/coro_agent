258cdcb34d0c6ab89b1d4b264f3e074d3c448420
wuyunwei.wyw <wuyunwei.wyw@alibaba-inc.com>
2023-09-21 18:37:37 +0800

    first commit

---
+++ coro_agent/coro_agent.py
@@ -0,0 +1,340 @@
+"""Chain that takes in an input and produces an action and action input."""
+from __future__ import annotations
+
+import io
+import logging
+import time
+from abc import abstractmethod
+from pathlib import Path
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+)
+
+from langchain.agents.agent_iterator import AgentExecutorIterator
+from langchain.agents.agent_types import AgentType
+from langchain.agents.tools import InvalidTool
+from langchain.callbacks.base import BaseCallbackManager
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForChainRun,
+    CallbackManagerForChainRun,
+)
+from langchain.schema import (
+    AgentAction,
+    AgentFinish,
+    OutputParserException,
+)
+from langchain.schema.language_model import BaseLanguageModel
+from langchain.tools.base import BaseTool
+from langchain.utilities.asyncio import asyncio_timeout
+from langchain.utils.input import get_color_mapping
+from langchain.agents.agent import ExceptionTool, AgentExecutor
+from langchain.load.serializable import Serializable
+
+logger = logging.getLogger(__name__)
+
+class CoroAgentSuspend(Serializable):
+    
+    output:str = None,
+        
+
+class CoroAgentExecutor(AgentExecutor):
+
+    _coro_status: Dict[str, Any] = {}
+
+    def save(self, file_path: Union[Path, str]) -> None:
+        s = self.to_json()
+        with io.open(file_path, 'w') as f:
+            f.write(s)
+
+    def save_agent(self, file_path: Union[Path, str]) -> None:
+        """Save the underlying agent."""
+        return self.agent.save(file_path)
+
+    def _return(
+        self,
+        output: AgentFinish,
+        intermediate_steps: list,
+        run_manager: Optional[CallbackManagerForChainRun] = None,
+    ) -> Dict[str, Any]:
+        if self._coro_status:
+            self._coro_status.clear()
+        if run_manager:
+            run_manager.on_agent_finish(output, color="green", verbose=self.verbose)
+        final_output = output.return_values
+        if self.return_intermediate_steps:
+            final_output["intermediate_steps"] = intermediate_steps
+        return final_output
+
+    async def _areturn(
+        self,
+        output: AgentFinish,
+        intermediate_steps: list,
+        run_manager: Optional[AsyncCallbackManagerForChainRun] = None,
+    ) -> Dict[str, Any]:
+        raise NotImplementedError
+
+    def _take_next_step(
+        self,
+        name_to_tool_map: Dict[str, BaseTool],
+        color_mapping: Dict[str, str],
+        inputs: Dict[str, str],
+        intermediate_steps: List[Tuple[AgentAction, str]],
+        run_manager: Optional[CallbackManagerForChainRun] = None,
+    ) -> Tuple[Union[AgentAction, AgentFinish], Union[str, CoroAgentSuspend, None]]:
+        """Take a single step in the thought-action-observation loop.
+
+        Override this to take control of how the agent makes and acts on choices.
+        """
+        try:
+            intermediate_steps = self._prepare_intermediate_steps(intermediate_steps)
+
+            # Call the LLM to see what to do.
+            output = self.agent.plan(
+                intermediate_steps,
+                callbacks=run_manager.get_child() if run_manager else None,
+                **inputs,
+            )
+        except OutputParserException as e:
+            if isinstance(self.handle_parsing_errors, bool):
+                raise_error = not self.handle_parsing_errors
+            else:
+                raise_error = False
+            if raise_error:
+                raise e
+            text = str(e)
+            if isinstance(self.handle_parsing_errors, bool):
+                if e.send_to_llm:
+                    observation = str(e.observation)
+                    text = str(e.llm_output)
+                else:
+                    observation = "Invalid or incomplete response"
+            elif isinstance(self.handle_parsing_errors, str):
+                observation = self.handle_parsing_errors
+            elif callable(self.handle_parsing_errors):
+                observation = self.handle_parsing_errors(e)
+            else:
+                raise ValueError("Got unexpected type of `handle_parsing_errors`")
+            output = AgentAction("_Exception", observation, text)
+            if run_manager:
+                run_manager.on_agent_action(output, color="green")
+            tool_run_kwargs = self.agent.tool_run_logging_kwargs()
+            observation = ExceptionTool().run(
+                output.tool_input,
+                verbose=self.verbose,
+                color=None,
+                callbacks=run_manager.get_child() if run_manager else None,
+                **tool_run_kwargs,
+            )
+            return output, observation
+        # If the tool chosen is the finishing tool, then we end and return.
+        if isinstance(output, AgentFinish):
+            return output, None
+        
+        if not isinstance(output, AgentAction):
+            raise ValueError('coro agent is not support for multi action')
+        
+        agent_action: AgentAction = output
+        
+        if run_manager:
+            run_manager.on_agent_action(agent_action, color="green")
+        # Otherwise we lookup the tool
+        if agent_action.tool in name_to_tool_map:
+            tool = name_to_tool_map[agent_action.tool]
+            return_direct = tool.return_direct
+            color = color_mapping[agent_action.tool]
+            tool_run_kwargs = self.agent.tool_run_logging_kwargs()
+            if return_direct:
+                tool_run_kwargs["llm_prefix"] = ""
+            # We then call the tool on the tool input to get an observation
+            observation = tool.run(
+                agent_action.tool_input,
+                verbose=self.verbose,
+                color=color,
+                callbacks=run_manager.get_child() if run_manager else None,
+                **tool_run_kwargs,
+            )
+        else:
+            tool_run_kwargs = self.agent.tool_run_logging_kwargs()
+            observation = InvalidTool().run(
+                {
+                    "requested_tool_name": agent_action.tool,
+                    "available_tool_names": list(name_to_tool_map.keys()),
+                },
+                verbose=self.verbose,
+                color=None,
+                callbacks=run_manager.get_child() if run_manager else None,
+                **tool_run_kwargs,
+            )
+        return agent_action, observation
+
+    async def _atake_next_step(
+        self,
+        name_to_tool_map: Dict[str, BaseTool],
+        color_mapping: Dict[str, str],
+        inputs: Dict[str, str],
+        intermediate_steps: List[Tuple[AgentAction, str]],
+        run_manager: Optional[AsyncCallbackManagerForChainRun] = None,
+    ) -> Union[AgentFinish, List[Tuple[AgentAction, str]]]:
+        raise NotImplementedError
+
+    def _call(
+        self,
+        inputs: Dict[str, str],
+        run_manager: Optional[CallbackManagerForChainRun] = None,
+    ) -> Dict[str, Any]:
+        """Run text through and get agent response."""
+        # Construct a mapping of tool name to tool for easy lookup
+        name_to_tool_map = {tool.name: tool for tool in self.tools}
+        # We construct a mapping from each tool to a color, used for logging.
+        color_mapping = get_color_mapping(
+            [tool.name for tool in self.tools], excluded_colors=["green", "red"]
+        )
+
+        intermediate_steps: List[Tuple[AgentAction, str]] = []
+        # When inputs arrive at a running agent, find the suspended agent and resume it.
+
+        if self._coro_status:
+            intermediate_steps.extend(self._coro_status['intermediate_steps'])
+            if not 'input' in inputs:
+                raise ValueError('coroutine agent mast run with params `input`')
+            
+            intermediate_steps.append((self._coro_status['last_agent_action'], inputs['input']))
+        else:
+            self._coro_status['agent_inputs'] = inputs
+        
+        # Let's start tracking the number of iterations and time elapsed
+        iterations = 0
+        time_elapsed = 0.0
+        start_time = time.time()
+        # We now enter the agent loop (until it returns something).
+        while self._should_continue(iterations, time_elapsed):
+            next_step_output, observation = self._take_next_step(
+                name_to_tool_map,
+                color_mapping,
+                self._coro_status['agent_inputs'],
+                intermediate_steps,
+                run_manager=run_manager,
+            )
+            if isinstance(next_step_output, AgentFinish):
+                return self._return(
+                    next_step_output, intermediate_steps, run_manager=run_manager
+                )
+
+            next_step_action = next_step_output
+
+            # When `CoroAgentSuspend` returned, suspend this agent and return
+            if isinstance(observation, CoroAgentSuspend):
+                self._coro_status['intermediate_steps'] = intermediate_steps
+                self._coro_status['last_agent_action'] = next_step_action
+                return { 
+                    'output': observation.output
+                }
+
+            intermediate_steps.append((next_step_action, observation))
+
+            # See if tool should return directly
+            tool_return = self._get_tool_return((next_step_output, observation))
+            if tool_return is not None:
+                return self._return(
+                    tool_return, intermediate_steps, run_manager=run_manager
+                )
+            iterations += 1
+            time_elapsed = time.time() - start_time
+        output = self.agent.return_stopped_response(
+            self.early_stopping_method, intermediate_steps, **self._coro_status['agent_inputs']
+        )
+        return self._return(output, intermediate_steps, run_manager=run_manager)
+
+    async def _acall(
+        self,
+        inputs: Dict[str, str],
+        run_manager: Optional[AsyncCallbackManagerForChainRun] = None,
+    ) -> Dict[str, str]:
+        raise NotImplementedError
+
+
+"""Load agent."""
+from typing import Any, Optional, Sequence
+
+from langchain.agents.loading import AGENT_TO_CLASS, load_agent
+from langchain.callbacks.base import BaseCallbackManager
+from langchain.schema.language_model import BaseLanguageModel
+from langchain.tools.base import BaseTool
+
+
+def initialize_coro_agent(
+    tools: Sequence[BaseTool],
+    llm: BaseLanguageModel,
+    agent: Optional[AgentType] = None,
+    callback_manager: Optional[BaseCallbackManager] = None,
+    agent_path: Optional[str] = None,
+    agent_kwargs: Optional[dict] = None,
+    *,
+    tags: Optional[Sequence[str]] = None,
+    **kwargs: Any,
+) -> CoroAgentExecutor:
+    """Load an agent executor given tools and LLM.
+
+    Args:
+        tools: List of tools this agent has access to.
+        llm: Language model to use as the agent.
+        agent: Agent type to use. If None and agent_path is also None, will default to
+            AgentType.ZERO_SHOT_REACT_DESCRIPTION.
+        callback_manager: CallbackManager to use. Global callback manager is used if
+            not provided. Defaults to None.
+        agent_path: Path to serialized agent to use.
+        agent_kwargs: Additional key word arguments to pass to the underlying agent
+        tags: Tags to apply to the traced runs.
+        **kwargs: Additional key word arguments passed to the agent executor
+
+    Returns:
+        An agent executor
+    """
+    tags_ = list(tags) if tags else []
+    if agent is None and agent_path is None:
+        agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION
+    if agent is not None and agent_path is not None:
+        raise ValueError(
+            "Both `agent` and `agent_path` are specified, "
+            "but at most only one should be."
+        )
+    if agent is not None:
+        if agent not in AGENT_TO_CLASS:
+            raise ValueError(
+                f"Got unknown agent type: {agent}. "
+                f"Valid types are: {AGENT_TO_CLASS.keys()}."
+            )
+        tags_.append(agent.value if isinstance(agent, AgentType) else agent)
+        agent_cls = AGENT_TO_CLASS[agent]
+        agent_kwargs = agent_kwargs or {}
+        agent_obj = agent_cls.from_llm_and_tools(
+            llm, tools, callback_manager=callback_manager, **agent_kwargs
+        )
+    elif agent_path is not None:
+        agent_obj = load_agent(
+            agent_path, llm=llm, tools=tools, callback_manager=callback_manager
+        )
+        try:
+            # TODO: Add tags from the serialized object directly.
+            tags_.append(agent_obj._agent_type)
+        except NotImplementedError:
+            pass
+    else:
+        raise ValueError(
+            "Somehow both `agent` and `agent_path` are None, "
+            "this should never happen."
+        )
+    return CoroAgentExecutor.from_agent_and_tools(
+        agent=agent_obj,
+        tools=tools,
+        callback_manager=callback_manager,
+        tags=tags_,
+        **kwargs,
+    )
